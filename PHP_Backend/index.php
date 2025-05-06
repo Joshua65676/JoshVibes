71288ff1e3ea1f2ad0cad774c9347d56577aa5dd
Okoro Joshua <Joshua65676@gmail.com>
2025-05-06 03:07:52 +0100

    Fist commit message

---
+++ PHP_Backend/index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JoshVibes</title>
+</head>
+<body>
+    <?php
+       echo "<h1>Hello World</h1>";
+       echo "<p>This is a simple PHP script.</p>";
+    ?>
+</body>
+</html>
